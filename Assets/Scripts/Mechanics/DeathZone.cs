02d70e3927ffa48bf9e39eba9d7229afe55bfcd8
YouMingYeh <b10705052@ntu.edu.tw>
2023-10-12 17:30:29 +0800

    .

--- Assets/Scripts/Mechanics/DeathZone.cs
+++ Assets/Scripts/Mechanics/DeathZone.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Platformer.Core;
 using Platformer.Gameplay;
+using Platformer.Model;
 using UnityEngine;
 using static Platformer.Core.Simulation;
 
@@ -12,14 +14,69 @@ namespace Platformer.Mechanics
     /// </summary>
     public class DeathZone : MonoBehaviour
     {
+        PlatformerModel model = Simulation.GetModel<PlatformerModel>();
         void OnTriggerEnter2D(Collider2D collider)
         {
-            var p = collider.gameObject.GetComponent<PlayerController>();
-            if (p != null)
+            GameObject collidedWith = collider.gameObject;
+            PlayerController playerController = collidedWith.GetComponent<PlayerController>();
+            Transform playerTransform = collidedWith.GetComponent<Transform>();
+            
+            if (playerController != null && collidedWith != null)
             {
+                UpdateFocus(collidedWith);
                 var ev = Schedule<PlayerEnteredDeathZone>();
                 ev.deathzone = this;
             }
         }
+        private void UpdateFocus(GameObject hitGameObject)
+        {
+            Debug.Log(hitGameObject.name);
+            if (hitGameObject.CompareTag("Player"))
+            {
+                GameObject selected = hitGameObject;
+                SpriteRenderer sprite = selected.GetComponent<SpriteRenderer>();
+
+                if (sprite != null)
+                {
+                    GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+                    Color newColor;
+                    PlayerController playerController;
+                    foreach (GameObject p in players)
+                    {
+                        if (p != null)
+                        {
+                            playerController = p.GetComponent<PlayerController>();
+                            SpriteRenderer othersprites = p.GetComponent<SpriteRenderer>();
+                            newColor = othersprites.color; // Create a copy of the current color.
+                            newColor.a = 0.5f; // Set the new alpha value.
+                            othersprites.color = newColor; // Assign the new color back to the SpriteRenderer.
+                            if (playerController != null)
+                            {
+                                Debug.Log("Changing control for player: " + playerController.gameObject.name);
+                                playerController.ChangeControlEnability(false);
+                            }
+
+                        }
+                    }
+
+                    newColor = sprite.color; // Create a copy of the current color.
+                    newColor.a = 1.0f; // Set the new alpha value.
+                    sprite.color = newColor; // Assign the new color back to the SpriteRenderer.
+                    playerController = selected.GetComponent<PlayerController>();
+
+
+                    if (playerController)
+                    {
+                        playerController.ChangeControlEnability(true);
+                        model.player = playerController;
+                    }
+
+
+                }
+
+            }
+        }
     }
+
+
 }
